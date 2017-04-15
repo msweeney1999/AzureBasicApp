f33ef22cc7c3e651f41510d74a1f6c368bb5a146
ms <ms@DESKTOP-2C37COU>
2017-04-15 15:25:10 -0400

    Added storage

---
+++ AzzureBasicApp/Controllers/StorageController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzzureBasicApp.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult Index()
+        {
+            var s = ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString;
+            var sa = CloudStorageAccount.Parse(s);
+
+            var tableClient = sa.CreateCloudTableClient();
+
+            var table = tableClient.GetTableReference("customer");
+
+            table.CreateIfNotExists();
+
+            var customer = new Customer(Guid.NewGuid())
+            {
+                First = "dave",
+                Last = "smith",
+                Email = "aaa.aol.com"
+
+            };
+
+            var insert = TableOperation.Insert(customer);
+            table.Execute(insert);
+            return View(customer);
+
+        }
+    }
+
+    public class Customer : TableEntity
+    {
+        public Customer(Guid id)
+        {
+            PartitionKey = "customer";
+            RowKey = id.ToString();
+        }
+
+        public Customer() { }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
